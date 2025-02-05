00454df681075cfc466d74399f13c7e8a514873f
TedWang06 <a790606@gmail.com>
2025-02-05 17:19:31 +0800

    加入專案檔案。

---
+++ WebApplication1/js/main.js
@@ -0,0 +1,32 @@
+﻿( function () {
+
+    init();
+
+    function init() {
+
+        const VueApp = {
+            data() {
+                return {
+                    message: ' Vue!'
+                }
+            },
+            mounted() {
+                getInitData();
+            }
+        }
+
+        Vue.createApp( VueApp ).mount( '#app' );
+    }
+
+    async function getInitData() {
+        debugger;
+        try {
+            const response = await axios.get( 'api/Customers' );
+            debugger;
+        }
+        catch ( e ) {
+            alert( 'get data error' );
+        }
+    }
+
+} )();
